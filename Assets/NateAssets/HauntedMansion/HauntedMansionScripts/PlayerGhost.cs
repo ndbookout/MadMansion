66c4ecf9212b131f332b88ead148f7ecb3b1761c
ndbookout <ndbookout@hotmail.com>
2015-11-17 19:25:34 -0700

    Initial commit Here is the Mansion

---
+++ Assets/NateAssets/HauntedMansion/HauntedMansionScripts/PlayerGhost.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGhost : MonoBehaviour
+{
+    public static PlayerGhost player;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        player = this;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MadMansion
+Whoohoo!
