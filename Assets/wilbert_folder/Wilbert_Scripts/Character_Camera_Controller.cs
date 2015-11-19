6084ce73566a4e8c6cd9d0da44044082dd9b085e
ptxwil <wilbert_rivera@hotmail.com>
2015-11-18 18:46:02 -0700

    This is the scene in progress with character controller being work on.

---
+++ Assets/wilbert_folder/Wilbert_Scripts/Character_Camera_Controller.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Camera_Controller : MonoBehaviour {
+    //Variable settings Public
+    public Transform target; //This the refrence to the ghost
+    public float lookSmooth = 0.09f; // how fast do we want to look at the ghost
+    public Vector3 offsetFromTargetGhost = new Vector3(0, 6, -8); // how far do we want the camera to be from ghost
+    public float xTilt = 10; //How far we want our camera to be rotate on the xAxis is going to be up or down
+
+    //Variable setting private
+    private Vector3 destination = Vector3.zero; //This is where we want our camera to move too
+    Character_Controller charController; // to acess the character rotation
+    float rotationVelocity = 0; //This is to set how fast our rotation moves in the camera
+
+    //Methods that we are using in our script
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<Character_Controller>())
+            {
+                charController = target.GetComponent<Character_Controller>();
+            }
+            else
+                Debug.LogError("The camera target need this character controller");
+        }
+        else
+            Debug.LogError("Your camera needs a target");
+    }
+
+    void LateUpdate()
+    {
+        //moves the camera
+        MoveToGhost();
+        //this rotates the camera
+        LookAtGhost();
+    }
+
+    void MoveToGhost()
+    {
+        destination = charController.TargetRotation * offsetFromTargetGhost;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAtGhost()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotationVelocity, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x,eulerYAngle, 0);
+    }
+
+
+
+}
---
+++ Assets/wilbert_folder/Wilbert_Scripts/Character_Controller.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controller : MonoBehaviour {
+    //These are our public serializable setting to control from the inspector in Unity
+    [System.Serializable]
+    public class MoveSettings // This will enclose our movement 
+    {
+        public float fowardVelocity = 12; //This is the velocity of the foward movement of the character
+        public float rotateVelocity = 100; //This is the rotation velocity of the character
+        public float jumpVelocity = 25; //This is the jump velocity of the character in the upward direction
+        public float distanceToGrounded = 0.1f; //This is the total distance the character is to the ground this will be a raycast that will return a distance this will be a bool check
+        public LayerMask ground; //This will check the the layer and what specific object he can jump from
+    }
+
+    [System.Serializable]
+    public class PhysSettings //These will encapsulate our physic variables
+    {
+        public float downwardAcceleration = 0.75f;
+    }
+
+    [System.Serializable]
+    public class InputSettings //These will ecapsualte our input settings
+    {
+        public float inputDelay = 0.1f;
+        public string FOWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
+
+
+    //These are the refrences to the classes this will make the classes modifiable from the inspector
+    public MoveSettings moveSetting = new MoveSettings();
+    public PhysSettings physSetting = new PhysSettings();
+    public InputSettings inputSetting = new InputSettings();
+
+    //Variable Settings Private
+    Vector3 ghostVelocity = Vector3.zero;
+    private Quaternion targetRotation; //Holds rotation to turn too
+    private Rigidbody ghostBody; //This will hold the ghost postion from the rigidbody
+    private float fowardInput; 
+    private float turnInput;
+    private float jumpInput;
+
+
+    //Methods that we are using in our script
+    void Start()
+    {
+        fowardInput = 0; //When we start our game the character will be at 0 movement
+        turnInput = 0; //When we start our game the character will have 0 rotation applied
+        jumpInput = 0;
+        targetRotation = transform.rotation;// This set the character rotation to the object the script is applied too
+
+        if (GetComponent<Rigidbody>())
+            ghostBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The character needs a rigidbody");
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+
+        ghostBody.velocity = transform.TransformDirection(ghostVelocity);
+        //ghostBody.velocity = ghostVelocity;
+    } 
+
+    void GetInput()
+    {
+        fowardInput = Input.GetAxis(inputSetting.FOWARD_AXIS);//interpolated will get any value from -1 to 1
+        turnInput = Input.GetAxis(inputSetting.TURN_AXIS);//interpolated will get any value from -1 to 1
+        jumpInput = Input.GetAxisRaw(inputSetting.JUMP_AXIS);//non-interpolate will only get values -1, 0, or 1 we only care that the value is greather than 0 because that will let us know that the player press the space bar
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(fowardInput) > inputSetting.inputDelay) //This is checking that our foward movement is greater than 0.1
+        {
+            //move
+            ghostVelocity.z = moveSetting.fowardVelocity * fowardInput;//So to move foward we only care about the .z vector of the character 
+        }
+        else
+            //zero velocity
+            ghostVelocity.z = 0; //This vector was create to modify only the axis we only care about base on the input
+    }
+    public Quaternion TargetRotation // This will allow us to access the rotation from our camera controller so that we can base the camera rotation of the character rotation
+    {
+        get { return targetRotation; }
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputSetting.inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSetting.rotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+        }
+        else
+            transform.rotation = targetRotation;
+    }
+
+    public bool Grounded()
+    {   //This will create an ray that will check the vector down, distance to groun will check the distance, ground will allows to jump
+        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distanceToGrounded, moveSetting.ground);
+    }
+
+    void Jump()
+    {
+        if (jumpInput > 0 && Grounded())
+        {
+            //jump
+            ghostVelocity.y = moveSetting.jumpVelocity;
+        }
+        else if (jumpInput == 0 && Grounded())
+        {
+            //zero out our veloctity.y
+            ghostVelocity.y = 0;
+        }
+        else
+        {
+            //decrease velocity.y
+            ghostVelocity.y -= physSetting.downwardAcceleration;
+        }
+    }
+
+}
