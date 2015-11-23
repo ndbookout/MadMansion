2249588d678d6a9162086c74ec14314fcc0c88c6
ptxwil <wilbert_rivera@hotmail.com>
2015-11-22 21:02:04 -0700

    Added the camera switching script, still working on it but its up and running.
    Signed-off-by: ptxwil <wilbert_rivera@hotmail.com>

---
+++ Assets/wilbert_folder/Wilbert_Scripts/SwitchCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+
+    public Camera[] cameras;
+    public Camera masterCamera;
+    public int cameraIndex;
+    public string CAMERA_AXIS = "SwitcCamera";
+    public float cameraInput;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cameraInput = 0;
+	    foreach (Camera cam in cameras)
+        {
+            cam.depth = -10;
+        }
+        masterCamera.depth = 1;
+        cameraIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+        SwitchCameraOnPress();
+        masterCamera.transform.position = cameras[cameraIndex].transform.position;
+        masterCamera.transform.rotation = cameras[cameraIndex].transform.rotation;
+	}
+
+    void GetInput()
+    {
+        cameraInput = Input.GetAxisRaw(CAMERA_AXIS);
+    }
+
+    void SwitchCameraOnPress()
+    {
+        if (cameraInput > 0)
+        {
+            cameraIndex = (cameraIndex + 1) % cameras.Length;
+        }
+    }
+}
