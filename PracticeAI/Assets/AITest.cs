031e00e158742e334a7d5c219cb410fc3ad96aa1
eschu <egschuller@gmail.com>
2015-11-24 14:22:13 -0700

    added practice AI and demo scene

---
+++ PracticeAI/Assets/AITest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AITest : MonoBehaviour {
+
+    Transform NPC;
+    public Transform target;
+    float moveSpeed = .1f;
+    float maxDistance;
+    
+
+    void Start ()
+    {
+        NPC = this.gameObject.transform;
+	}
+
+    RaycastHit hit;
+   
+    void FixedUpdate()
+    {
+        Ray NPCRay = new Ray(NPC.position, Vector3.forward);
+        Physics.Raycast(NPCRay, out hit, maxDistance = 5f);
+        if (hit.collider.tag == "Door")
+        {
+            target = hit.transform;
+            Vector3 distanceNPCtoTarget = (NPC.position - target.position);
+            if (distanceNPCtoTarget.magnitude > 1)
+            {
+                NPC.LookAt(target);
+                NPC.position = Vector3.Lerp(NPC.position, target.position, Time.deltaTime * moveSpeed);
+            }
+        }
+    }
+	void Update ()
+    {
+        
+        
+        //target = GameObject.FindWithTag("Door").transform;
+
+        
+
+    }
+
+    
+}
