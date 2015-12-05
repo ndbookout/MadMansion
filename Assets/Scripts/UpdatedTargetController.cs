92db2f0723b6f9b242a22521fadcf5a2202ff57c
eschu <egschuller@gmail.com>
2015-12-05 13:22:39 -0700

    added AI and updated list script

---
+++ Assets/Scripts/UpdatedTargetController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UpdatedTargetController : MonoBehaviour
+{
+
+    public static List<GameObject> NorthWingDoors = new List<GameObject>();
+    public static List<GameObject> SouthWingDoors = new List<GameObject>();
+    public static List<GameObject> WestWingDoors = new List<GameObject>();
+    public static List<GameObject> HubDoors = new List<GameObject>();
+
+    public GameObject NorthWingTrigger;
+    public GameObject SouthWingTrigger;
+    public GameObject WestWingTrigger;
+
+    
+    
+
+    void Start ()
+    {
+        foreach (GameObject tar in GameObject.FindGameObjectsWithTag("NorthWing"))
+        {
+            NorthWingDoors.Add(tar);
+        }
+
+        foreach (GameObject tar in GameObject.FindGameObjectsWithTag("SouthWing"))
+        {
+            SouthWingDoors.Add(tar);
+        }
+
+        foreach (GameObject tar in GameObject.FindGameObjectsWithTag("WestWing"))
+        {
+            WestWingDoors.Add(tar);
+        }
+
+        foreach (GameObject tar in GameObject.FindGameObjectsWithTag("HubDoor"))
+        {
+            HubDoors.Add(tar);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    
+
+}
