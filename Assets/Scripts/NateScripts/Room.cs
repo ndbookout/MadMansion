89b3781624c01652cd2738e534b5bc8aafbc9c1f
ndbookout <ndbookout@hotmail.com>
2015-12-05 13:30:56 -0700

    12-5-15, Procedural Rooms and a ton of random textures.

---
+++ Assets/Scripts/NateScripts/Room.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Rooms
+{
+
+    public class Room : MonoBehaviour
+    {
+        public GameObject doorObject;
+        public GameObject[] doorSpaces; //0 = south, 1 = west, 2 = north, 3 = east
+
+        private Direction[] doorDirections;
+
+        //public List<GameObject> targetList;
+        //public List<GameObject> doorList;
+
+        public void SetData(params Direction[] directions)
+        {
+            doorDirections = directions;
+
+            InitializeRoom();
+        }
+
+        void InitializeRoom()
+        {
+            foreach (Direction dir in doorDirections)
+            {
+                if (dir == Direction.South)
+                {
+                    SetDoor(0);
+                }
+                else if (dir == Direction.West)
+                {
+                    SetDoor(1);
+                }
+                else if (dir == Direction.North)
+                {
+                    SetDoor(2);
+                }
+                else if (dir == Direction.East)
+                {
+                    SetDoor(3);
+                }
+            }            
+        }
+
+        void SetDoor(int doorNum)
+        {
+            Vector3 doorPosition = doorSpaces[doorNum].transform.position;
+            Destroy(doorSpaces[doorNum]);
+            //Instantiate(doorObject, doorPosition, Quaternion.identity);
+        }
+
+        //public List<GameObject> GetTargetList()
+        //{
+        //    return targetList;
+        //}
+
+        //public void SetTargetList(GameObject tar)
+        //{
+        //    targetList.Add(tar);
+        //}
+
+        //public void SetDoorList(GameObject door)
+        //{
+        //    doorList.Add(door);
+        //}
+    }
+}
