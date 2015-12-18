0907ef314c9b882f21ab92b8be9d5c933a20eb1b
eschu <egschuller@gmail.com>
2015-12-18 16:32:02 -0700

    added start menu

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public Texture2D texture;
+    static LoadingScreen instance;
+    public static GameObject button;
+
+    void Awake()
+   {
+         if (instance)
+         {
+             Destroy(gameObject);
+             hide();
+             return;
+         }
+         button = GameObject.FindGameObjectWithTag("Button");
+         instance = this;    
+         gameObject.AddComponent<GUITexture>().enabled = false;
+         GetComponent<GUITexture>().texture = texture;
+         transform.position = new Vector3(0.5f, 0.5f, 1f);
+         //DontDestroyOnLoad(this);
+         
+    }
+
+    //void Update()
+    //{
+    //    if (!Application.isLoadingLevel)
+    //        hide();
+    //}
+
+    public static void show()
+    {
+         if (!InstanceExists()) 
+         {
+             return;
+         }
+         instance.GetComponent<GUITexture>().enabled = true;
+        HideButton();
+        
+     }
+         
+     public static void hide()
+    {
+        if (!InstanceExists())
+        {
+            return;
+        }
+        instance.GetComponent<GUITexture>().enabled = false;
+    }
+
+    public static void HideButton()
+    {
+        button.GetComponent<Image>().enabled = false;
+    }
+     static bool InstanceExists()
+     {
+        if (!instance)
+        {
+            return false;
+        }
+        return true;
+
+     }
+
+}
---
+++ Assets/Scripts/StartButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour {
+
+	public void startPressed()
+    {
+        LoadingScreen.show();
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitPressed()
+    {
+        Application.Quit();
+    }
+}
