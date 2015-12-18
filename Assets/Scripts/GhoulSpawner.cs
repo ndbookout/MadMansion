fcabd4b301694f740b9ef81a3c48698fc22f424c
eschu <egschuller@gmail.com>
2015-12-17 18:35:08 -0700

    ghouls spawn in empty rooms, npc and ghoul both animated

---
+++ Assets/Scripts/GhoulSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhoulSpawner : MonoBehaviour {
+
+    public GameObject ghoulPrefab;
+    public float ghoulSpawnTimer = 45f;
+
+
+	void Start ()
+    {
+        StartCoroutine(SpawnGhoul());
+    }
+	
+	
+	void Update ()
+    {
+	
+	}
+    IEnumerator SpawnGhoulsMoreFrequentlyOverTime()
+    {
+        yield return new WaitForSeconds(20f);
+        if (ghoulSpawnTimer > 8)
+        {
+            ghoulSpawnTimer--;
+            StartCoroutine(SpawnGhoulsMoreFrequentlyOverTime());
+        }
+    }
+    IEnumerator SpawnGhoul()
+    {
+        Instantiate(ghoulPrefab, this.transform.position, this.transform.rotation);
+        yield return new WaitForSeconds(ghoulSpawnTimer);
+        StartCoroutine(SpawnGhoul());
+    }
+}
