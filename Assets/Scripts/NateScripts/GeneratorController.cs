538656bbcbb17e0b5e08073d69c551ab25ced822
ndbookout <ndbookout@hotmail.com>
2015-12-17 17:00:58 -0700

    3 hubs!

---
+++ Assets/Scripts/NateScripts/GeneratorController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorController : MonoBehaviour
+{
+    public GameObject[] generators;
+
+	// Use this for initialization
+	void Start ()
+    {
+        new Task(TriggerGenerators());
+	}
+
+    IEnumerator TriggerGenerators()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (generators[i].activeSelf == false)
+            {
+                generators[i].SetActive(true);
+                yield return new WaitForSeconds(3);
+            }
+        }
+    }
+	
+}
