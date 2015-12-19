158fd70771731927076a318b6afed254b9cf5324
ptxwil <wilbert_rivera@hotmail.com>
2015-12-19 03:48:02 -0700

    Add Paused Menu to scene for tomorrow presentation.
    Signed-off-by: ptxwil <wilbert_rivera@hotmail.com>

--- Assets/Scripts/StartButtonController.cs
+++ Assets/Scripts/StartButtonController.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.SceneManagement;
 
-public class StartButtonController : MonoBehaviour {
+//public class StartButtonController : MonoBehaviour {
 
-	public void startPressed()
-    {
-        LoadingScreen.show();
-        SceneManager.LoadScene(1);
-    }
+//	public void startPressed()
+//    {
+//        LoadingScreen.show();
+//        SceneManager.LoadScene(1);
+//    }
 
-    public void quitPressed()
-    {
-        Application.Quit();
-    }
-}
+//    public void quitPressed()
+//    {
+//        Application.Quit();
+//    }
+//}
