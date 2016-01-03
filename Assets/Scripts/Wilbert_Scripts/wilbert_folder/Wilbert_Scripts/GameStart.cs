50db27323b6beb5d10e35148eedc8a71431e6a71
ptxwil <wilbert_rivera@hotmail.com>
2016-01-03 13:26:28 -0700

    trying to grab finish project
    Signed-off-by: ptxwil <wilbert_rivera@hotmail.com>

--- Assets/Scripts/Wilbert_Scripts/wilbert_folder/Wilbert_Scripts/GameStart.cs
+++ Assets/Scripts/Wilbert_Scripts/wilbert_folder/Wilbert_Scripts/GameStart.cs
@@ -5,30 +5,22 @@ using System.Collections;
 public class GameStart : MonoBehaviour {
 
     public Button startText; //This is our game start button 
-    public Button exitText; //this is for us to exit the game
-    public Button retryText; //this is for us to exit the game
 
 
     // Use this for initialization
     void Start ()
     {
         startText = startText.GetComponent<Button>(); // Same as the top
-        exitText = exitText.GetComponent<Button>(); // same at the top
-        exitText = retryText.GetComponent<Button>(); // same at the top
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 
     public void StartLevel() //this will load the first stage when play is hit
     {
         Application.LoadLevel(1);
-    }
-
-    public void ExitGame() //This will exit the game and pop the exit menu when hit
-    {
-        Application.Quit();
     }
 }
