08ba08655f6ed0028d8554905a1979b707f49ebe
ptxwil <wilbert_rivera@hotmail.com>
2016-01-03 13:27:17 -0700

    Still trying to get files for finish project
    Signed-off-by: ptxwil <wilbert_rivera@hotmail.com>

---
+++ Assets/Scripts/Wilbert_Scripts/wilbert_folder/Wilbert_Scripts/GameEnd.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // This is added because we are using the Unity UI in our code to get all the fucntions
+using System.Collections;
+
+public class GameEnd : MonoBehaviour
+{
+
+    public Button exitGame; //this is for us to exit the game
+    public Button retryText; //this is for us to exit the game
+
+
+    // Use this for initialization
+    void Start()
+    {
+        exitGame = exitGame.GetComponent<Button>(); // same at the top
+        retryText = retryText.GetComponent<Button>(); // same at the top
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ExitGame() //This will exit the game and pop the exit menu when hit
+    {
+        Application.Quit();
+    }
+
+    public void RetryGame() //This will exit the game and pop the exit menu when hit
+    {
+        Application.LoadLevel(1);
+    }
+}
